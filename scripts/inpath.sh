61a5a20da92856b1e33d8b51e72896499f4a50be
Dean Hamilton <deanhamilton11@gmail.com>
2019-03-21 09:09:52 -0400

    Working on inpath scripts

--- scripts/inpath.sh
+++ scripts/inpath.sh
@@ -11,10 +11,8 @@ in_path()
   cmd=$1        ourpath=$2         result=1
   oldIFS=$IFS   IFS=":"
 
-  for directory in "$ourpath"
+  for directory in “$ourpath”
   do
-    echo $directory
-    echo $cmd
     if [ -x $directory/$cmd ] ; then
       result=0 # If we're here, we found the command.
     fi
